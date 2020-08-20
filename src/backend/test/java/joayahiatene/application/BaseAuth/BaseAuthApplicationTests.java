2987fa7ca48ac2410800793d4b6b6ad8e16537c1
ymokrane <mokrane_yahiatene@web.de>
2020-08-20 15:54:20 +0200

    added backend and project structure

---
+++ src/backend/test/java/joayahiatene/application/BaseAuth/BaseAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package joayahiatene.application.BaseAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
