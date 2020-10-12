5d1adab792a1667f7f5e39a04602f651e874994b
ymokrane <mokrane_yahiatene@web.de>
2020-10-12 21:16:50 +0200

    fixed project structure

---
+++ src/main/java/de/joayahiatene/baseauth/BaseauthApplication.java
@@ -0,0 +1,13 @@
+package de.joayahiatene.baseauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseauthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/joayahiatene/baseauth/BaseauthApplicationTests.java
@@ -0,0 +1,13 @@
+package de.joayahiatene.baseauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
